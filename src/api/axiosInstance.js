26a9b4cdd1ecd51c4c27fffd9dbc02c04416c24b
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-07-03 00:45:17 +0700

    small changes in vercel json n api path

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
-  //   baseURL: "https://weimoodapi.hizkiajonathanbudiana.my.id",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://quizapi.hizkiajonathanbudiana.my.id",
   withCredentials: true,
 });
 
